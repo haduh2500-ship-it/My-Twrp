713380d6ede5e8d696fdbda3da496c786f915f6f
haduh2500-ship-it <haduh2500@gmail.com>
2026-07-15 10:42:00 +0500

    Update omni_BH502G.mk

--- omni_BH502G.mk
+++ omni_BH502G.mk
@@ -4,12 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from core build configurations. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from BH502G device configuration
 $(call inherit-product, device/herofun/BH502G/device.mk)
